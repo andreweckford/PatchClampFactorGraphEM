fafb2e247bfb1b996aab5b86e52df29983c85b58
Andrew Eckford <aeckford@gmail.com>
2022-01-20 11:19:49 -0500

    Updated script generator

--- resultHandling/scriptGeneratorCFTR.py
+++ resultHandling/scriptGeneratorCFTR.py
@@ -7,14 +7,19 @@ Created on Mon Aug  3 08:29:17 2020
 """
 
 import numpy as np
+import sys
 
-numTimeInstants_v = [2000]
-maxEMIterations_v = [40]
+# python3 main.py -r=CFTR -n=20000 -i=400 -c=0.8 -p=0.198 -l -v=0.1 -ev > noisy_result14_20000_400_08_0198_l.csv
+
+num_cores = int(sys.argv[1]) # number of cores is the first command line argument
+coreID = int(sys.argv[2]) # which core is the second command line argument
+
+
+numTimeInstants_v = [20000]
+maxEMIterations_v = [400]
 confidence_v = [0.8]
-C1aXP_v = np.arange(0.05,0.1,0.01)
-numRuns = 1
-num_cores = 1
-coreID = 0
+C1aXP_v = np.arange(0.01,0.2,0.002)
+numRuns = 10
 confidenceDigits = 1
 C1aXPDigits = 3
 
@@ -46,11 +51,12 @@ for numTimeInstants in numTimeInstants_v:
                         scriptString += ' -i=' + str(maxEMIterations) 
                         scriptString += ' -c=0.' + mysf(confidence,confidenceDigits)
                         scriptString += ' -p=0.' + mysf(C1aXP,C1aXPDigits) 
+                        scriptString += ' -v=0.02 -ev'
                         scriptString += ' -l > result' + str(run) 
                         scriptString += '_' + str(numTimeInstants)
                         scriptString += '_' + str(maxEMIterations) 
                         scriptString += '_0' + mysf(confidence,confidenceDigits)
-                        scriptString += '_0' + mysf(C1aXP,C1aXPDigits) + '_l.csv'
+                        scriptString += '_0' + mysf(C1aXP,C1aXPDigits) + '_0.02_l.csv'
                         
                         print(scriptString)
 
